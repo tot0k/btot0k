d6fdf672ee426fd183904e0e27b0058e62cea6b5
tot0k <redtotok@gmail.com>
2017-04-15 21:26:10 +0200

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 import socket, string
 
 HOST = "irc.twitch.tv"
-NICK = "bott0k"
+NICK = "nom de votre bot"
 PORT = 6667
-PASS = "oauth:5if2vwp3jolm2p6isqxdqdvpffxj33"
+PASS = "oauth: votre oauth ici"
 CHANNEL = "#t0t0k"
